07a88c93abe587cc07df53978227234b8074a9aa
Muhammad <muhammadhullah23@gmail.com>
2022-04-09 19:57:43 -0400

    Rocket Prefab

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+
+// UnityEngine is the namespace that all of the MonoBehaviour content lives within
 using UnityEngine;
 
+// Our movement class is inheritting content created in MonoBehaviour 
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
