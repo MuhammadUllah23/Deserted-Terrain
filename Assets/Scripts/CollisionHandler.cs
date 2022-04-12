06d15a2c59184fe68380bfcdd38d87fbaeff6214
Muhammad <muhammadhullah23@gmail.com>
2022-04-12 02:53:33 -0400

    when player bumps into friendly

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,5 +3,11 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    void OnCollisionEnter(Collision other) {
+            switch (other.gameObject.tag) {
+                case "Friendly":
+                    Debug.Log("Hello Friend!")
+                    break;
+            }
+    }
 }
