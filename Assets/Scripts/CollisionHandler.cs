0d7497436dac1c23590098d2dcfbb1fd3d230729
Muhammad <muhammadhullah23@gmail.com>
2022-04-12 03:22:19 -0400

    LoadNextLevel to load next scene

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,13 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    int currentSceneIndex;
     void OnCollisionEnter(Collision other) {
             switch (other.gameObject.tag) {
                 case "Friendly":
                     Debug.Log("Hello Friend!");
                     break;
                 case "Finish":
-                    Debug.Log("YAAAYYY. YOU MADE IT!");
+                    LoadNextLevel();
                     break;
                 default:
                     Debug.Log("You died :(");
@@ -20,7 +21,12 @@ public class CollisionHandler : MonoBehaviour
     }
 
     void ReloadLevel() {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel() {
+        int nextSceneIndex = currentSceneIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
