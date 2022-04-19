e5014843d9916a8236e342319fb5a11c64d57a8b
Muhammad <muhammadhullah23@gmail.com>
2022-04-19 01:49:06 -0400

    add thrust particles to rocket prefab movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,6 +12,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotateThrust = 500f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem leftBoosterParticle;
+    [SerializeField] ParticleSystem rightBoosterParticle;
+    [SerializeField] ParticleSystem mainThrustParticle;
     
     // CACHE - e.g. references for readability or speed
     Rigidbody objectRigidbody;
